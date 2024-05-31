bfd92e07b7e4d543e4e24d3c9f7a474a5140fc64
Bilal Asghar <bilalasghar586@gmail.com>
2024-05-31 11:27:53 +0500

    changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  exports: [
+  ],
+  providers: [
+    provideClientHydration()
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
