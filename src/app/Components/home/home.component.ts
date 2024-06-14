070857627ff7422dce7fa640780a3ad863c7d663
Bilal Asghar <bilalasghar586@gmail.com>
2024-06-14 11:54:52 +0500

    header-menu

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+declare var bootstrap: any;
+
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,23 @@ import { Subscription, interval } from 'rxjs';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  @ViewChild('dialog')dialog!: ElementRef;
+  isModalOpen : boolean = false;
+  isDarkMode: boolean = false;
 
   constructor(){
 
   }
 
+  ngAfterViewInit(): void {
+ 
+  }
+
   ngOnInit(): void {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
   }
 
   // downloadCV(){
@@ -21,12 +32,32 @@ export class HomeComponent implements OnInit {
   //   const link = document.createElement('a');
   //   link.setAttribute('target', '_blank');
   //   link.setAttribute('href', filePath);
-  //   link.setAttribute('download', 'Resume.pdf');
+  //   link.setAttribute('download', 'Resume.pdf');s
   //   document.body.appendChild(link);
   //   link.click();
   //   link.remove();
   // }
+  
+  openModal() {
+    this.dialog.nativeElement.show();
+    // this.text.nativeElement.textContent = '';
+  }
 
+  closeModal() {
+    this.dialog.nativeElement.close();
+    // this.text.nativeElement.innerHTML += '"close the dialog" closed the dialog.<br/>';
+  }
+
+  onCancel(event: any) {
+    // this.text.nativeElement.innerHTML += 'cancel event happened<br/>';
+  }
+
+  onClose(event: any) {
+    // this.text.nativeElement.innerHTML += 'close event happened<br/>';
+  }
+  toggleMode() {
+    this.isDarkMode = !this.isDarkMode;
+  }
 
 
 }
