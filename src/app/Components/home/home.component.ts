2b067c7ac1bff4309cef97e44faca3788d7fc4c0
Bilal Asghar <bilalasghar586@gmail.com>
2024-06-14 12:18:40 +0500

    new Menu header

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -8,9 +8,9 @@ declare var bootstrap: any;
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  @ViewChild('dialog')dialog!: ElementRef;
-  isModalOpen : boolean = false;
-  isDarkMode: boolean = false;
+  // @ViewChild('dialog')dialog!: ElementRef;
+  // isModalOpen : boolean = false;
+  // isDarkMode: boolean = false;
 
   constructor(){
 
@@ -21,10 +21,10 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-      return new bootstrap.Popover(popoverTriggerEl)
-    })
+    // var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    // var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    //   return new bootstrap.Popover(popoverTriggerEl)
+    // })
   }
 
   // downloadCV(){
@@ -38,26 +38,26 @@ export class HomeComponent implements OnInit {
   //   link.remove();
   // }
   
-  openModal() {
-    this.dialog.nativeElement.show();
-    // this.text.nativeElement.textContent = '';
-  }
+  // openModal() {
+  //   this.dialog.nativeElement.show();
+  //   // this.text.nativeElement.textContent = '';
+  // }
 
-  closeModal() {
-    this.dialog.nativeElement.close();
-    // this.text.nativeElement.innerHTML += '"close the dialog" closed the dialog.<br/>';
-  }
+  // closeModal() {
+  //   this.dialog.nativeElement.close();
+  //   // this.text.nativeElement.innerHTML += '"close the dialog" closed the dialog.<br/>';
+  // }
 
-  onCancel(event: any) {
-    // this.text.nativeElement.innerHTML += 'cancel event happened<br/>';
-  }
+  // onCancel(event: any) {
+  //   // this.text.nativeElement.innerHTML += 'cancel event happened<br/>';
+  // }
 
-  onClose(event: any) {
-    // this.text.nativeElement.innerHTML += 'close event happened<br/>';
-  }
-  toggleMode() {
-    this.isDarkMode = !this.isDarkMode;
-  }
+  // onClose(event: any) {
+  //   // this.text.nativeElement.innerHTML += 'close event happened<br/>';
+  // }
+  // toggleMode() {
+  //   this.isDarkMode = !this.isDarkMode;
+  // }
 
 
 }
